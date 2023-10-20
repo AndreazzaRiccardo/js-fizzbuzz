4c5613dd0ea618dbfeb82556123cb6a65b8bd40e
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-10-20 17:07:24 +0200

    add: color layout

--- js/script.js
+++ js/script.js
@@ -4,18 +4,23 @@ let liElem = "";
 for (i = 1; i <= 100; i++) {
 
     let message;
+    let colorClass;
 
     if ((i % 3 == 0) && (i % 5 == 0)) {
         message = "FizzBuzz";
+        colorClass = "red";
     } else if (i % 5 == 0) {
-        message = "Buzz"
+        message = "Buzz";
+        colorClass = "blue";
     } else if (i % 3 == 0) {
-        message = "Fizz"
+        message = "Fizz";
+        colorClass = "green";
     } else {
-        message = i
+        message = i;
+        colorClass = "yellow";
     }
         
-    liElem += `<li class="col">${message}</li>`
+    liElem += `<li class="col ${colorClass}">${message}</li>`;
 }
 
-ulRow.innerHTML = liElem
+ulRow.innerHTML = liElem;
